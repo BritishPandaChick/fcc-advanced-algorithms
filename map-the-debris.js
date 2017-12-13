5bc766f877324fe3e442eb363b461865125314da
BritishPandaChick <sbartley1080@gmail.com>
2017-12-12 21:52:57 -0500

    Add map-the-debris algorithm

---
+++ map-the-debris.js
@@ -0,0 +1,16 @@
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+
+  return arr.reduce(function(acc, obj) {
+    //use reduce function using acc and obj
+    var orbPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + obj.avgAlt, 3) / GM));
+    //formula for orbit attached to var orbPeriod
+    acc.push({name: obj.name, orbitalPeriod: orbPeriod});
+    //create new entry in acc
+    return acc;
+  }, []);
+  //put in an array
+}
+
+orbitalPeriod([{name: "sputnik", avgAlt: 35873.5553}]);
